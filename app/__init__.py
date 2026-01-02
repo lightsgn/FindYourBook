31da8cb18db8f2eda69607a8afdb676339b7baaa
derinminabektas <derinminabektas444@gmail.com>
2026-01-02 23:15:39 +0300

    düzeltmeler

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 
 
 def create_app():
-    # Şablonların (HTML) ve Statik dosyaların (CSS/Resim) yerini gösteriyoruz:
     app = Flask(__name__,
                 template_folder='ui/templates',
                 static_folder='ui/static')
 
-    app.config['SECRET_KEY'] = 'dev'  # Veya config'den alıyorsan oradan çek
+    app.config['SECRET_KEY'] = 'dev'
 
-    # --- Buradan sonrası senin eski kodunla aynı kalabilir ---
+
 
     from . import db
     db.init_app(app)
